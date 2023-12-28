92aa390fad8761f3028d4fb455e94f4a2a7f6fed
RdoEgito <33696073+RdoEgito@users.noreply.github.com>
2023-12-28 19:27:45 -0300

    Versão inicial

---
+++ CestaBasica/CestaBasica.Client/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace CestaBasica.Client
+{
+    public struct Item
+    {
+        public Item(int selectedQuantity, string selectedItem)
+        {
+            SelectedQuantity = selectedQuantity;
+            SelectedItem = selectedItem;
+        }
+
+        public int SelectedQuantity { get; set; }
+        public string SelectedItem { get; set; }
+    }
+}
